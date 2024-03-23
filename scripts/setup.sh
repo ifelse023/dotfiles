9f59a677288484970e22b7090d39920a54532c9d
ifelse023 <ifelse023@gmail.com>
2024-03-23 01:46:56 +0100

    yep

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,8 @@ tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
 cd ..
 
-sudo pacman -Syu --needed base-devel mold sccache reflector python ccache neovim lld paru
+sudo pacman -Syu --needed base-devel mold sccache reflector python ccache neovim lld paru rustup
 
 sudo cp -r ~/dotfiles/system/makepkg.conf /etc/makepkg.conf
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup default nightly
