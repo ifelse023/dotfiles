6f3ab51a3eb8277840708cc18c1418e1db874f75
ifelse023 <ifelse023@gmail.com>
2025-07-29 16:43:09 +0200

    :)

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    set -U fish_greeting ""
+    set fish_greeting ""
     starship init fish | source
     zoxide init fish --cmd cd | source
     source ~/.config/fish/atuin.fish
@@ -7,63 +7,15 @@ end
 fish_add_path /home/wasd/architect/scripts
 
 abbr -a c chezmoi
-abbr -a n vim
+abbr -a n helix
+abbr -a x helix
 abbr -a j just
 abbr -a py python
+abbr -a wl wl-copy
 
 abbr -a .. 'cd ..'
 abbr -a ... 'cd ../..'
 abbr -a .... 'cd ../../..'
 abbr -a ..... 'cd ../../../..'
 
-alias cat="bat --paging=never"
-alias diff="diff --color=auto"
-alias update='sudo pacman -Syu && paru -Syu'
-alias vim='neovide'
-alias ls='eza -h --git --icons --color=auto --group-directories-first -s extension'
-alias la='eza -a --git --icons --color=auto --group-directories-first'
-alias ll='eza -l --git --icons --color=auto --group-directories-first'
-alias lt='eza -T --git --icons --color=auto --level=2 --group-directories-first'
-alias l.="eza -a | rg '^\.'"
-
-alias readelf='llvm-readelf'
-alias objdump='llvm-objdump'
-alias nm='llvm-nm'
-
-function x
-    uwsm app neovide
-end
-
-function xx
-    set file (fd --type f --hidden --exclude .git | fzf --preview 'bat --color=always --style=numbers {}')
-    if test -n "$file"
-        uwsm app -- neovide "$file"
-    end
-end
-
-function xd
-    set dir (fd --type d --exclude .git | fzf --height 20 --preview 'eza --tree --level=1 --color=always {}')
-    if test -n "$dir"
-        cd "$dir"
-    end
-end
-
-function xc
-    set file (fd --type f --hidden --exclude .git . ~/.local/share/chezmoi | fzf --preview 'bat --color=always --style=numbers {}')
-    if test -n "$file"
-        neovide "$file"
-    end
-end
-
-function se
-    sudoedit $argv
-end
-
-function exit_if_empty
-    if test -z (commandline)
-        exit
-    end
-end
-
-source ~/.config/fish/env.fish
 source ~/.local/share/cargo/env.fish
--- dot_config/private_fish/functions/functions.fish
+++ dot_config/private_fish/functions/functions.fish
@@ -1,11 +1,56 @@
-function x
-    uwsm app neovide
+function update --description 'Update Arch Linux system packages (pacman and paru)'
+    command sudo pacman -Syu && command paru -Syu
+end
+
+function diff --wraps diff --description 'Alias for diff --color=auto'
+    command diff --color=auto $argv
+end
+
+function ls --wraps eza --description 'List files with eza (common options)'
+    command eza -h --git --icons --color=auto --group-directories-first -s extension $argv
+end
+
+function la --wraps eza --description 'List all files with eza (including hidden)'
+    command eza -a --git --icons --color=auto --group-directories-first $argv
+end
+
+function ll --wraps eza --description 'List files with eza (long format)'
+    command eza -l --git --icons --color=auto --group-directories-first $argv
+end
+
+function lt --wraps eza --description 'List files with eza (tree view, level 2)'
+    command eza -T --git --icons --color=auto --level=2 --group-directories-first $argv
+end
+
+function l. --wraps eza --description 'List only dotfiles in the current directory'
+    # Arguments are passed to eza before being piped to ripgrep (rg).
+    command eza -a $argv | command rg '^\.'
+end
+
+function cat --wraps bat --description 'Alias for bat --paging=never'
+    command bat --paging=never $argv
+end
+
+function nvim --wraps helix --description 'Alias for helix'
+    command helix $argv
+end
+
+function readelf --wraps llvm-readelf --description 'Alias for llvm-readelf'
+    command llvm-readelf $argv
+end
+
+function objdump --wraps llvm-objdump --description 'Alias for llvm-objdump'
+    command llvm-objdump $argv
+end
+
+function nm --wraps llvm-nm --description 'Alias for llvm-nm'
+    command llvm-nm $argv
 end
 
 function xx
     set file (fd --type f --hidden --exclude .git | fzf --preview 'bat --color=always --style=numbers {}')
     if test -n "$file"
-        uwsm app -- neovide "$file"
+        uwsm app -- helix "$file"
     end
 end
 
@@ -19,7 +64,7 @@ end
 function xc
     set file (fd --type f --hidden --exclude .git . ~/.local/share/chezmoi | fzf --preview 'bat --color=always --style=numbers {}')
     if test -n "$file"
-        neovide "$file"
+        helix "$file"
     end
 end
 
@@ -78,11 +123,6 @@ function cpr
     rsync -aHAX --info=NAME,PROGRESS2 --human-readable --no-inc-recursive -- $argv
 end
 
-function echo-variable --no-scope-shadowing
-    if set -q -- $argv
-        set varname $argv
-    else
-        set varname (echo $argv | string upper)
-    end
-    eval 'echo $'$varname
+function chezmoi-edit
+    chezmoi edit --verbose --apply
 end
