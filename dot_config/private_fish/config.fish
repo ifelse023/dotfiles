5c95bfe1792b929b81344d3508e20b5dee3ef197
ifelse023 <162376926+ifelse023@users.noreply.github.com>
2025-08-30 15:09:36 +0200

    :)

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -33,3 +33,5 @@ abbr -a ..... 'cd ../../../..'
 
 source ~/.config/fish/functions.fish
 
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+set -gx PATH $HOME/.cabal/bin /home/wasd/.ghcup/bin $PATH # ghcup-env
